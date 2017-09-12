b4539026643e506de0efdf08b8b645aa957ee98b
wangyanqing <625259292@qq.com>
2017-09-12 13:45:53 +0800

    add

---
+++ test/ChatClient_Win/ChatClient_Win/Friends.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatClient_Win
+{
+    public class FData
+    {
+        public string Name { get; set; }
+        public string Desc { get; set; }
+    }
+
+    public class Data
+    {
+        public string Name { get; set; }
+
+        List<FData> _FList = new List<FData>();
+        public List<FData> FList { get { return _FList; } }
+    }
+
+    /// <summary>
+    /// Friends.xaml 的交互逻辑
+    /// </summary>
+    public partial class Friends : Page
+    {
+        public Friends()
+        {
+            InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<Data> datalist = new List<Data>();
+            for (int i = 0; i < 5; i++)
+            {
+                Data data = new Data();
+                data.Name = "aaa" + i;
+
+                for (int j = 0; j < 3; j++)
+                {
+                    FData fdata = new FData();
+                    fdata.Name = "bbb" + j;
+                    fdata.Desc = "desc" + j;
+                    data.FList.Add(fdata);
+                }
+                datalist.Add(data);
+            }
+
+            treeView.ItemsSource = datalist;
+        }
+    }
+}
---
+++ test/ChatClient_Win/ChatClient_Win/Login.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatClient_Win
+{
+    /// <summary>
+    /// Login.xaml 的交互逻辑
+    /// </summary>
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.SizeToContent = SizeToContent.WidthAndHeight;
+            //Application.Current.MainWindow.Width = 300;
+            //Application.Current.MainWindow.Height = 600;
+            //var navwin = Application.Current.MainWindow as NavigationWindow;
+            //navwin.
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Friends());
+        }
+    }
+}
